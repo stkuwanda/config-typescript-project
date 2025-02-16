9c22d09dfb984705848f55794f74f77bedff0c06
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-16 23:02:20 +0200

    setup configuration for debugging with chrome tools in vscode

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,24 @@
-console.log('Run....');
+// console.log('Run....');
 
-type User = { name: string; age: number, id: string };
-const user: User = { name: 'Simbarashe', age: 34, id: 'ehuiughhuihi23rbubjb' };
+// type User = { name: string; age: number, id: string };
+// const user: User = { name: 'Simbarashe', age: 34, id: 'ehuiughhuihi23rbubjb' };
+
+let appId = 'abc';
+const button = document.querySelector('button')!;
+
+function add(n1: number, n2: number) {
+	if (n1 + n2 > 0) {
+		return n1 + n2;
+	}
+	return;
+}
+
+function clickHandler(message: string) {
+	// let userName = 'Max';
+	console.log('Clicked! ' + message);
+}
+
+// a comment
+if (button) {
+	button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+}
